0d4ef2df09c8d1a31462904a4ea2120983357a2b
Gepsonka <expeletix.botond@gmail.com>
2021-10-26 18:39:24 +0200

    Restructure folder structure

---
+++ trade_operations/stock_class/stock_class_base.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import json
+
+
+class Stock:
+    """Represents a stock
+    
+    Params
+    ------
+
+    stock_name : Name of the stock, loaded from 500stock.csv.
+    time_interval : The amount of data we want to fetch from the IEX cloud api from the given stock (5y/2y/2m/...) given as a string.
+    
+    Class variables
+    ---------------
+
+    BASE_URL: Base url of our api. We will concatenate the rest of the URI to this string
+    API_TOKEN: Token for the api passed to the URI as an url query (?token=cscsdsdccycy)
+
+    """
+    BASE_URL = 'https://sandbox.iexapis.com/stable'
+    API_TOKEN = os.environ.get('API_TOKEN')
+
+    def __init__(self,stock_name,period_of_time) -> None:
+        self.stock_name=stock_name
+        self.period_of_time=period_of_time
+
+    def download_stock_data(self):
+        """Download data from server"""
+        response=requests.get(f'{self.BASE_URL}/stock/aapl/chart/{self.period_of_time}?token={self.API_TOKEN}')
+        if response.status_code!=200:
+            raise ConnectionError('Cannot get the data from the server: '+ str(response.status_code) +', '+self.stock_name)
+        else:
+            return response.json()
+    
+    def save_downloaded_data(self):
+        """Write downloaded data into /static/stocks forder in stock_{stock name}.json format"""
+        with open('static/stocks/'+ self.period_of_time +'/stock_'+ self.stock_name +'.json','w') as jsonfile:
+            try:
+                jsonfile.write(json.dumps(self.download_stock_data()))
+            except:
+                raise IOError('Cannot write stock charts (OS error)')
+            finally:
+                jsonfile.close()
